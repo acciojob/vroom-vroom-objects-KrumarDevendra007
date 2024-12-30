25eb11d194341e5f3d726d64b05800373da87441
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-31 00:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	make = this.make;
+	model = this.model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Method added to Car prototype
+Car.prototype.getMakeModel = function(){
+	return `${this.make} ${this.model}`;
+}
+
+function SportsCar(make, model, topSpeed) {
+	make = this.make;
+	model = this.model;
+	topSpeed = this.topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed = funtion() {
+	return `${this.topSpeed}`;
+}
 
 // Do not change the code below
 window.Car = Car;
